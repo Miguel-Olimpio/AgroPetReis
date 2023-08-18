822036f9e70c2f275d36a06680452a0628f4f356
Miguel-Olimpio <miguel.netto@engenharia.ufjf.br>
2023-08-18 17:59:46 -0300

    kghgh

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.use(
         }),
         cookie:{
             secure: false,
-            maxAge: 360000,
+            maxAge: 60000,
             expires: new Date(Date.now() + 360000),
             httpOnly: true
         }
