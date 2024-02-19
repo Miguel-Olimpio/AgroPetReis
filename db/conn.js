71d7dab7af47ce939d5bad864d26cefc81d58910
Miguel-Olimpio <miguel.netto@engenharia.ufjf.br>
2024-02-19 19:33:15 -0300

    teste

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
     dialect: 'mysql'
 });
 
