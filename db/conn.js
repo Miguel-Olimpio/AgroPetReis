3947b3090833c4382bd5b357bce8e070538737f1
Miguel-Olimpio <miguel.netto@engenharia.ufjf.br>
2024-02-21 23:06:34 -0300

    teste 13

--- db/conn.js
+++ db/conn.js
@@ -1,10 +1,10 @@
 import dotenv from 'dotenv';
-dotenv.config({path: '/etc/environment'});
+dotenv.config();
 
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`,`${process.env.DB_PASSWORD}`, {
-    host: `${process.env.DB_HOST}`,
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER,process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'mysql'
 });
 
