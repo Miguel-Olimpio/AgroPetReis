dfd62c4394b4ea06b1f869b75ddd644d9d9f5efe
Miguel-Olimpio <miguel.netto@engenharia.ufjf.br>
2024-02-24 17:43:49 -0300

    mais um teste

--- db/conn.js
+++ db/conn.js
@@ -5,7 +5,8 @@ import { Sequelize } from 'sequelize';
 
 const db = new Sequelize('agropetvetreis', 'agroPetAdmin','Nettao260695', {
     host: 'agropetreis.c7i0ks280v4u.us-east-1.rds.amazonaws.com',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    port: '3306'
 });
 
 try {
